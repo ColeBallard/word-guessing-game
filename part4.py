80b41d7801c0b1040d7b3e95ebc6e3b370037191
Cole Ballard <cole.ballard97@gmail.com>
2022-06-30 09:54:18 -0500

    finished part 4

--- part4.py
+++ part4.py
@@ -5,7 +5,10 @@ with open("assets/words_alpha.txt", "r") as f:
 
 print("Word Guessing Game")
 
+wins = 0
+losses = 0
 game_over = False
+round_over = False
 while not game_over:
     random_word = random.choice(random_word_list)
     random_word_list.remove(random_word)
@@ -18,44 +21,71 @@ while not game_over:
     guessed_letters = []
     win = False
 
-    print("Your word is " + hidden_word + " and you have " + str(7 - player_guesses) + " guesses left.")
-    player_guess = input("press 1 to guess letter, 2 to guess word, 3 to exit: ")
+    while not round_over:
+        print("Your word is " + hidden_word + " and you have " + str(7 - player_guesses) + " guesses left.")
+        player_guess = input("press 1 to guess letter, 2 to guess word, 3 to exit: ")
 
-    if player_guess == "1":
-        letter = input("")
-        if letter in guessed_letters or len(letter) != 1:
-            print("invalid guess, try again")
-            continue
-        else:
-            guessed_letters.append(letter)
-            player_guesses += 1
-            i = 0
-            for l in random_word:
-                if letter == l:
-                    hidden_word_list = [char for char in hidden_word]
-                    hidden_word_list[i] = letter
-                    hidden_word = "".join(hidden_word_list)
-                i += 1
-            if hidden_word == random_word:
-                win = True
-    elif player_guess == "2":
-        word = input("")
-        if(len(word) != len(random_word)):
-            print("invalid guess, try again")
-            continue
-        else:
-            player_guesses += 1
-            if(word == random_word):
-                win = True
+        if player_guess == "1":
+            letter = input("")
+
+            if letter in guessed_letters or len(letter) != 1:
+                print("invalid guess, try again")
+                continue
+
             else:
-                print("wrong guess")
-    elif player_guess == "3":
-        break
-    else:
-        print("invalid option, try again")
-    if player_guesses == 7 or win == True:
+                guessed_letters.append(letter)
+                player_guesses += 1
+                i = 0
+
+                for l in random_word:
+                    if letter == l:
+                        hidden_word_list = [char for char in hidden_word]
+                        hidden_word_list[i] = letter
+                        hidden_word = "".join(hidden_word_list)
+                    i += 1
+
+                if hidden_word == random_word:
+                    win = True
+
+        elif player_guess == "2":
+            word = input("")
+
+            if(len(word) != len(random_word)):
+                print("invalid guess, try again")
+                continue
+
+            else:
+                player_guesses += 1
+
+                if(word == random_word):
+                    win = True
+
+                else:
+                    print("wrong guess")
+
+        elif player_guess == "3":
+            losses += 1
+            round_over = True
+
+        else:
+            print("invalid option, try again")
+
+        if win:
+            print("Nice win! It took you " + str(player_guesses) + " guesses.")
+            wins += 1
+            round_over = True
+
+        elif player_guesses == 7:
+            print("You lost. The word was " + random_word + ". Better luck next time!")
+            losses += 1
+            round_over = True
+
+    print("Your score is " + str(wins) + " wins and " + str(losses) + " losses.")
+
+    play_again = input("press 1 to play again, 2 to exit: ")
+
+    if play_again == "1":
+        round_over = False
+    
+    elif play_again == "2":
         game_over = True
-if win:
-    print("Congratulations on winning! It took you " + str(player_guesses) + " guesses.")
-else:
-    print("You lost. The word was " + random_word + ". Better luck next time!")
